bba34e5dc30a2bae10c304ea85100dd767b16d67
Saodat <saodat.riskiyeva@gmail.com>
2024-05-13 22:48:43 -0400

    removing commented out lines

--- starter/script.js
+++ starter/script.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct Number!';
-// console.log(document.querySelector('.message').textContent);
-
-// console.log(document.querySelector('.number').textContent);
-// console.log(document.querySelector('.score').textContent);
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
@@ -45,17 +35,6 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
-  // When guess is too low
-  // } else if (guess < secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = 'Too low!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = 'You lost the game!';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-  // }
 });
 
 // Coding Challenge #1
